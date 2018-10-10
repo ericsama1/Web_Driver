3f0dee0a96b46bd7eb0a4b65c80b1ae25d2d721f
Eric Enomoto <eric.enomimoto@gmail.com>
2018-10-10 10:55:45 -0300

    ejemplo de como realizar una busqueda en google

---
+++ examples/search_google.py
@@ -0,0 +1,16 @@
+import sys; sys.path.append('..')
+from Web_Driver import Driver
+from time import sleep
+
+
+def main():
+    driver = Driver()
+    driver.url('https://google.com')
+    sleep(1)
+    driver.send_text_to_input('id', 'lst-ib', 'Hello World!')
+    driver.send_enter_key('id', 'lst-ib')
+    sleep(4)
+
+
+if __name__ == '__main__':
+    main()
