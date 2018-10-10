772aeea3f00ca9452ae7e2295683d6de0bff16e4
Eric Enomoto <eenomoto@msa.com.ar>
2018-10-10 09:59:42 -0300

    Arreglo el ejemplo para obtener el logo

--- examples/get_image.py
+++ examples/get_image.py
@@ -1,13 +1,15 @@
 """Metodo para obtener el logo del google"""
-
+import sys; sys.path.append('..')
 from Web_Driver import Driver
 from time import sleep
+
 
 def main():
     driver = Driver()
     driver.url('https://google.com')
     sleep(1)
-    driver.get_image('id', 'hplogo', '/tmp/prueba.png')
+    driver.get_image('id', 'hplogo', 'google_logo.png')
+
 
 if __name__ == "__main__":
     main()
