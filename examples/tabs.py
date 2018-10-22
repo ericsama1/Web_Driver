2e917c27612ce9a1ae77a3e93620dd3908d84cbb
Eric Enomoto <eric.enomimoto@gmail.com>
2018-10-22 15:26:12 -0300

    Modifico el ejemplo del manejo de tabs

--- examples/tabs.py
+++ examples/tabs.py
@@ -5,15 +5,15 @@ from time import sleep
 
 def main():
     driver = Driver()
-    driver.url('https://google.com')
-    driver.new_tab('https://youtube.com')
+    driver.url('https://youtube.com')
+    driver.new_tab('https://google.com')
     driver.new_tab('https://facebook.com')
     driver.change_tab(driver.tabs()[0])
     sleep(1)
+    driver.close_tab()
+    sleep(1)
     driver.send_text_to_input('id', 'lst-ib', 'Hello World!')
     driver.send_enter_key('id', 'lst-ib')
-    sleep(1)
-    driver.close_tab()
     sleep(1)
     driver.close()
 
