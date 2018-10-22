56bad0b75d55857eea88943ae4c3b3b8ce2ff4a5
Eric Enomoto <eric.enomimoto@gmail.com>
2018-10-22 11:43:24 -0300

    Agrego un ejemplo de como usar las opciones del browser

---
+++ examples/use_options.py
@@ -0,0 +1,15 @@
+
+from Web_Driver import Driver
+from time import sleep
+
+def main():
+    driver = Driver(browser='firefox', incognito='True')
+    driver.url('https://google.com')
+    sleep(1)
+    driver.send_text_to_input('id', 'lst-ib', 'Hello World!')
+    driver.send_enter_key('id', 'lst-ib')
+    sleep(1)
+    driver.close()
+
+if __name__ == '__main__':
+    main()
